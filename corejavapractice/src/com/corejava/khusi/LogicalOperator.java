4f0b90246c92d5e9d4ed727408fbca71d9d096b7
khpawar <khushi02pawar@gmail.com>
2021-06-30 15:26:20 +0530

    code added for logical operator

---
+++ corejavapractice/src/com/corejava/khusi/LogicalOperator.java
@@ -0,0 +1,12 @@
+package com.corejava.khusi;
+
+public class LogicalOperator {
+   public static void main(String args[]){
+	  boolean a = true;
+	  boolean b = false;
+	  
+	  System.out.println("a & b= " + (a&b));
+	  System.out.println("a || b= " + (a||b));
+	  System.out.println("a !(a & b)= " + !(a&b));
+   }
+}
