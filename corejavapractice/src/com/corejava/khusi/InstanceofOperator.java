9b1d0dec5aa08645d41bb228e86ce0a3ead47f0e
khpawar <khushi02pawar@gmail.com>
2021-06-30 15:51:59 +0530

    code added for operator

---
+++ corejavapractice/src/com/corejava/khusi/InstanceofOperator.java
@@ -0,0 +1,14 @@
+package com.corejava.khusi;
+
+public class InstanceofOperator {
+   public static void main(String agrs[]){
+	   String name = "monica";
+	    
+	   boolean result = name instanceof String;
+	   System.out.println(result);
+	   
+    }
+	
+}
+
+
