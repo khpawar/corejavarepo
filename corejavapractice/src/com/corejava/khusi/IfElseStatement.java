a07e24bf453c985891ccc844e1b77df6c8bca231
khpawar <khushi02pawar@gmail.com>
2021-07-01 14:15:17 +0530

    code added for if else statement

---
+++ corejavapractice/src/com/corejava/khusi/IfElseStatement.java
@@ -0,0 +1,15 @@
+package com.corejava.khusi;
+
+public class IfElseStatement {
+	public static void main(String args[]){
+		int x = 30;
+
+		if (x < 20){
+			System.out.println(x);
+			System.out.print("This is if statement");
+		}else {
+			System.out.println(x);
+			System.out.println("This is else statement");
+		}
+	}
+}
---
+++ corejavapractice/src/com/corejava/khusi/IfStatement.java
@@ -0,0 +1,12 @@
+package com.corejava.khusi;
+
+public class IfStatement {
+    public static void main(String args[]){
+    	int x = 10;
+    	 if (x < 20){
+    		 System.out.println(x);
+    		 System.out.println("This is if statement");
+    	}
+	}
+
+}
