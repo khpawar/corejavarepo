f6b28d67d60737ac6fcc1230927cee9fa950c8c9
khpawar <khushi02pawar@gmail.com>
2021-07-01 14:56:28 +0530

    code added for if else if statement

---
+++ corejavapractice/src/com/corejava/khusi/IfelseifStatement.java
@@ -0,0 +1,32 @@
+package com.corejava.khusi;
+
+public class IfelseifStatement {
+	public static void main(String args[]){
+		int x = 30;
+
+		if(x == 10){
+			System.out.print("value of x is 10");
+		}
+		else if(x == 20){
+			System.out.println("value of x is 20");
+			System.out.print("This is an else if block");
+		}
+		else if(x == 30){
+			System.out.println("value of x is 30");
+			System.out.print("This is an else if block");
+		}
+		else {
+			System.out.print("This is else statement");
+
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
