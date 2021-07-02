56f368c1df1acb4df7f1ce0cb8ef300d6a2699ad
khpawar <khushi02pawar@gmail.com>
2021-07-02 15:03:17 +0530

    code added for local variable

---
+++ corejavapractice/src/com/corejava/khusi/LocalVariable.java
@@ -0,0 +1,10 @@
+package com.corejava.khusi;
+
+public class LocalVariable {
+	public static void main(String args[]){
+		int a= 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("sum of a and b is: "+c);
+	}
+}
