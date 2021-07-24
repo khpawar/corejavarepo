9af94903db41eeeedabd08f96c3780c0186c3e4e
khpawar <khushi02pawar@gmail.com>
2021-07-24 17:44:57 +0530

    code added for method1

---
+++ corejavapractice/src/com/corejava/khusi/Method1.java
@@ -0,0 +1,10 @@
+package com.corejava.khusi;
+
+public class Method1 {
+	public static void printMessage(){
+		System.out.println("Hello Everyone");
+	}
+	public static void main(String[] args){
+		printMessage();
+	}
+}
