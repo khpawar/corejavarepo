59348671c255b4a5a1c2dc744bf1d5f19ea0f81f
khpawar <khushi02pawar@gmail.com>
2021-08-10 17:41:45 +0530

    code added for characterexam

---
+++ corejavapractice/src/com/corejava/khusi/CharacterExam.java
@@ -0,0 +1,15 @@
+package com.corejava.khusi;
+
+public class CharacterExam {
+	public static void main(String args[]){
+		System.out.println(Character.isLetter('c'));
+		System.out.println(Character.isLetter('5'));
+		System.out.println(Character.isLowerCase('c'));
+		System.out.println(Character.isLowerCase('C'));
+		System.out.println(Character.isUpperCase('T'));
+		System.out.println(Character.toUpperCase('m'));
+		System.out.println(Character.toString('c'));
+	}
+}
+
+
