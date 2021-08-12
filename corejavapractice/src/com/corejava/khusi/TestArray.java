fd9112a9feb57a529157c1b0f9e8c49fdd4da8e5
khpawar <khushi02pawar@gmail.com>
2021-08-12 16:19:35 +0530

    code added for testarray

---
+++ corejavapractice/src/com/corejava/khusi/TestArray.java
@@ -0,0 +1,11 @@
+package com.corejava.khusi;
+
+public class TestArray {
+	public static void main(String[] args){
+		double[]myList = {1.9,2.9,3.4,3.5};
+		//print all the array elements
+		for(double element:myList){
+			System.out.println(element);
+		}
+	}
+}
