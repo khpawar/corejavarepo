b79d618ae6d16253e050c55e9ba5205d50878e1b
khpawar <86618950+khpawar@users.noreply.github.com>
2021-09-16 16:22:43 +0530

    Create MathTest.java
    new code added

---
+++ corejavapractice/src/com/corejava/khusi/MathTest.java
@@ -0,0 +1,10 @@
+package com.corejava.khusi;
+
+public class MathTest {
+	static int players = 0;
+	public static void main(String [] args) {
+		System.out.println("players online: " + players++);
+		System.out.println("The value of platers is " + players);
+		System.out.println("The value of players is now " + ++players);
+	}
+}
